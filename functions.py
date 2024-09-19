7c212634da5137e0ea56cbba04f42e4543d2b632
Josie Welsh <jowe9773@colorado.edu>
2024-09-19 08:19:53 -0600

    adding the basics to the functions file

---
+++ functions.py
@@ -0,0 +1,52 @@
+#functions.py
+
+#import neccesary packages and modules
+import tkinter as tk
+from tkinter import filedialog
+import pandas as pd
+
+
+class FileFunctions:
+    """Class contains methods for managing files"""
+
+    def __init__(self):
+        print("Initialized file managers")
+
+    def load_dn(self, purpose):
+        """this function opens a tkinter GUI for selecting a 
+        directory and returns the full path to the directory 
+        once selected
+        
+        'purpose' -- provides expanatory text in the GUI
+        that tells the user what directory to select"""
+
+        root = tk.Tk()
+        root.withdraw()
+        directory_name = filedialog.askdirectory(title = purpose)
+
+        return directory_name
+
+    def load_fn(self, purpose):
+        """this function opens a tkinter GUI for selecting a 
+        file and returns the full path to the file 
+        once selected
+        
+        'purpose' -- provides expanatory text in the GUI
+        that tells the user what file to select"""
+
+        root = tk.Tk()
+        root.withdraw()
+        filename = filedialog.askopenfilename(title = purpose)
+
+        return filename
+
+class CountDataFunctions:
+    def __init__(self):
+        print("Initialized CountDataFunctions")
+
+    def load_jam_data(self, filename):
+        "function to load in jam level data from an experiment"
+
+
+
+
